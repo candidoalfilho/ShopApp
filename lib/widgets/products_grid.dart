c0e6787866687b17f76e9bb9d082ef8ae677eae1
Candido Alfredo <candidoalfilho@hotmail.com>
2021-05-03 21:33:24 -0300

    Working with State

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -5,10 +5,13 @@ import 'package:shopapp/providers/products.dart';
 import './product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
+  final bool showFavs;
+
+  ProductsGrid(this.showFavs);
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
@@ -16,10 +19,10 @@ class ProductsGrid extends StatelessWidget {
         value: products[i],
         //create: (c) =>products[i],
         child: ProductItem(
-          // products[i].id,
-          // products[i].title,
-          // products[i].imageUrl,
-        ),
+            // products[i].id,
+            // products[i].title,
+            // products[i].imageUrl,
+            ),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
