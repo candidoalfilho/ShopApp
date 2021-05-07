294bd26b1234a5aac43b8e289ebe32637c62ff2f
Candido Alfredo <candidoalfilho@hotmail.com>
2021-05-07 10:49:25 -0300

    Showing Alerts Dialog

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -37,6 +37,29 @@ class CartItem extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove the item from the cart?'),
+            actions: [
+              TextButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+              ),
+              TextButton(
+                child: Text('Yes'),
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+              ),
+            ],
+          ),
+        );
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
@@ -57,7 +80,8 @@ class CartItem extends StatelessWidget {
                 ),
               ),
               title: Text(title),
-              subtitle: Text('Total: \$${(price * quantity).toStringAsFixed(2)}'),
+              subtitle:
+                  Text('Total: \$${(price * quantity).toStringAsFixed(2)}'),
               trailing: Text('$quantity x')),
         ),
       ),
