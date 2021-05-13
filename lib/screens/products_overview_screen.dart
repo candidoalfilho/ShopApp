861403933aabca3445734f6f09356890d6ec89ed
Candido Alfredo <candidoalfilho@hotmail.com>
2021-05-13 11:41:43 -0300

    Transforming fetched data

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -21,6 +21,8 @@ class ProductsOverviewScreen extends StatefulWidget {
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavorites = false;
   var _isInit = true;
+  var _isLoading = false;
+
   @override
   void initState() {
     //Provider.of<Products>(context).fetchAndSetProducts(); //Not going to work
@@ -33,7 +35,12 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   @override
   void didChangeDependencies() {
     if(_isInit){
-      Provider.of<Products>(context).fetchAndSetProducts();
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Products>(context).fetchAndSetProducts().then((_){
+        _isLoading = false;
+      });
     }
     _isInit = false;
     super.didChangeDependencies();
@@ -79,7 +86,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         ],
       ),
       drawer: AppDrawer(),
-      body: ProductsGrid(_showOnlyFavorites),
+      body: _isLoading ? Center(child: CircularProgressIndicator(),) : ProductsGrid(_showOnlyFavorites),
     );
   }
 }
