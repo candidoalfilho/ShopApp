4b521e9b38151914bc9fcb658513203828c28b15
Candido Alfredo <candidoalfilho@hotmail.com>
2021-05-26 19:23:55 -0300

    Allowing Users to log in

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -7,17 +7,28 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(String email, String password, String urlSegment) async{
     final url = Uri.parse(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDpt8SJRMH9IM2dD60Sb59EAg9HAm8ypuw");
+        "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDpt8SJRMH9IM2dD60Sb59EAg9HAm8ypuw");
     final response = await http.post(
       url,
-      body: json.encode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      },),
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signupNewUser');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'verifyPassword');
+
   }
 }
