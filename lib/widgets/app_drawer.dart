e1e8836fd1c91c8b0437109e91e94650ffb5c0fa
Candido Alfredo <candidoalfilho@hotmail.com>
2021-06-03 10:26:56 -0300

    Adding a Logout functionality

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:shopapp/screens/user_products_screen.dart';
 import '../screens/orders_screen.dart';
+import '../providers/auth.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -33,7 +35,19 @@ class AppDrawer extends StatelessWidget {
           leading: Icon(Icons.edit),
           title: Text('Manage Products'),
           onTap: () {
-            Navigator.of(context).pushReplacementNamed(UserProductsScreen.routeName);
+            Navigator.of(context)
+                .pushReplacementNamed(UserProductsScreen.routeName);
+          },
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.exit_to_app),
+          title: Text('Logout'),
+          onTap: () {
+            Navigator.of(context).pop();
+            //Navigator.of(context)
+                //.pushReplacementNamed(UserProductsScreen.routeName);
+            Provider.of<Auth>(context, listen: false).logout();
           },
         ),
       ]),
